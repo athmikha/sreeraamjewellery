47dc12beda6bd63f8fd3b7de967ec7fe3ba38c54
athmikha <athmikhasudharsan@gmail.com>
2025-12-24 13:04:54 +0530

    .

--- jewellery/src/App.js
+++ jewellery/src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import CategoryPage from "./components/CategoryPage";
 import SubCategoryPage from "./components/SubCategoryPage";
